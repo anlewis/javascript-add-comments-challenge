d60b5632e6e927779f8a98024d6b3a8e41458874
Anna Lewis <anna.noel.lewis@gmail.com>
2018-04-27 14:00:48 -0600

    Adds comments, but does not include author

--- source/addComment.js
+++ source/addComment.js
@@ -1,25 +1,47 @@
 $(document).ready(function () {
   $("#new_comment_button").click(function (event) {
     event.preventDefault()
+
     $("body").append(
       $("<form/>", {
         id: 'new_comment',
         style: 'display: inline-block;'
       }).append(
         $("<textarea/>", {
-          id: "new_comment",
+          id: "comment",
           placeholder: 'Your comment here...'
         }), $("<br/>"), $("<input/>", {
           type: 'text',
           id: 'authorName',
           placeholder: 'author'
         }), $("<input/>", {
+          id: 'create_comment_button',
           type: 'submit',
-          value: 'Submit Comment'
+          value: 'Create Comment'
         })
       )
     )
-  $("form#new_comment").css("display", "default");
-  $("#new_comment_button").css("display", "none");
+
+    $("form#new_comment").css("display", "inline-block");
+    $("#new_comment_button").css("display", "none");  
+  })
+
+  $(document).on('click', '#create_comment_button', function (event) {
+    event.preventDefault()
+
+    let comment = $("textarea#comment").val();
+    let author = $("input#authorName").val();
+
+    $("ul").append(
+      $("<li/>").append(
+        comment
+      , $("<span/>", {
+        class: "author"
+      }, author)
+      )
+    )
+
+    $("form#new_comment").remove();
+    $("#new_comment_button").css("display", "inline-block");
   })
 })
