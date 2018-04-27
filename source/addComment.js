6106588323987f81a9e484ee27601fa34b051a4a
Anna Lewis <anna.noel.lewis@gmail.com>
2018-04-27 13:07:05 -0600

    Make New Comment Button work

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-  // add your code here
+$(document).ready(function () {
+  $("#new_comment_button").click(function (event) {
+    event.preventDefault()
+    $("body").append(
+      $("<form/>", {
+        id: 'new_comment',
+        style: 'display: inline-block;'
+      }).append(
+        $("<textarea/>", {
+          id: "new_comment",
+          placeholder: 'Your comment here...'
+        }), $("<br/>"), $("<input/>", {
+          type: 'text',
+          id: 'authorName',
+          placeholder: 'author'
+        }), $("<input/>", {
+          type: 'submit',
+          value: 'Submit Comment'
+        })
+      )
+    )
+  $("form#new_comment").css("display", "default");
+  $("#new_comment_button").css("display", "none");
+  })
 })
